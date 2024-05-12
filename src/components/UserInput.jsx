8f44e6792b76e112a6ac215383ba11c226578b23
helllify <hellify.io@icloud.com>
2024-05-12 05:36:44 +0300

    mend

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,4 +1,22 @@
-export default function UserInput({ onChange, userInput }) {
+import { useState } from 'react';
+
+export default function UserInput() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput((prevUserInput) => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue,
+            };
+        });
+    }
+
     return (
         <section id="user-input">
             <div className="input-group">
@@ -9,7 +27,7 @@ export default function UserInput({ onChange, userInput }) {
                         required
                         value={userInput.initialInvestment}
                         onChange={(event) =>
-                            onChange('initialInvestment', event.target.value)
+                            handleChange('initialInvestment', event.target.value)
                         }
                     />
                 </p>
@@ -20,7 +38,7 @@ export default function UserInput({ onChange, userInput }) {
                         required
                         value={userInput.annualInvestment}
                         onChange={(event) =>
-                            onChange('annualInvestment', event.target.value)
+                            handleChange('annualInvestment', event.target.value)
                         }
                     />
                 </p>
@@ -33,7 +51,7 @@ export default function UserInput({ onChange, userInput }) {
                         required
                         value={userInput.expectedReturn}
                         onChange={(event) =>
-                            onChange('expectedReturn', event.target.value)
+                            handleChange('expectedReturn', event.target.value)
                         }
                     />
                 </p>
@@ -43,7 +61,9 @@ export default function UserInput({ onChange, userInput }) {
                         type="number"
                         required
                         value={userInput.duration}
-                        onChange={(event) => onChange('duration', event.target.value)}
+                        onChange={(event) =>
+                            handleChange('duration', event.target.value)
+                        }
                     />
                 </p>
             </div>
